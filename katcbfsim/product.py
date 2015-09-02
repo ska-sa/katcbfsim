73650e4b3b34850918217fddf2b70cacbd22ba96
Bruce Merry <bmerry@ska.ac.za>
2015-09-02 11:14:11 +0200

    Code restructuring

---
+++ katcbfsim/product.py
@@ -0,0 +1,115 @@
+from __future__ import print_function, division
+import trollius
+import copy
+import logging
+import time
+from trollius import From
+from katsdptelstate import endpoint
+
+
+logger = logging.getLogger(__name__)
+
+
+@trollius.coroutine
+def wait_until(future, when, loop=None):
+    """Like :meth:`trollius.wait_for`, but with an absolute timeout."""
+    def ready(*args):
+        if not waiter.done():
+            waiter.set_result(None)
+
+    if loop is None:
+        loop = trollius.get_event_loop()
+    waiter = trollius.Future(loop=loop)
+    timeout_handle = loop.call_at(when, ready)
+    # Ensure the that future is really a future, not a coroutine object
+    future = trollius.async(future, loop=loop)
+    future.add_done_callback(ready)
+    try:
+        result = yield From(waiter)
+        if future.done():
+            raise trollius.Return(future.result())
+        else:
+            future.remove_done_callback(ready)
+            future.cancel()
+            raise trollius.TimeoutError()
+    finally:
+        timeout_handle.cancel()
+
+
+class Subarray(object):
+    def __init__(self):
+        self.antennas = []
+        self.sync_time = time.time()
+        self.accumulation_length = 0.5
+
+
+class Product(object):
+    def __init__(self, subarray, name, bandwidth, channels, loop=None):
+        self.name = name
+        self.subarray = subarray
+        self.destination = [endpoint.Endpoint('0.0.0.0', 0)]
+        self.bandwidth = bandwidth
+        self.channels = channels
+        self.center_frequency = 1412000000
+        self._active = False
+        self._capture_future = None
+        self._stop_future = None
+        if loop is None:
+            self._loop = trollius.get_event_loop()
+        else:
+            self._loop = loop
+
+    @property
+    def active(self):
+        return self._active
+
+    def activate(self):
+        assert not self.active
+        # Freeze the subarray so that we're not affected by changes on the
+        # master copy
+        self.subarray = copy.deepcopy(self.subarray)
+        # TODO: send the initial packets
+        self._active = True
+
+    def capture_start(self):
+        assert self.active
+        if self._capture_future is not None:
+            logger.warn('Ignoring attempt to start capture when already running')
+            return
+        self._stop_future = trollius.Future(loop=self._loop)
+        self._capture_future = trollius.async(self._capture(), loop=self._loop)
+
+    @trollius.coroutine
+    def capture_stop(self):
+        if self._capture_future is None:
+            logger.warn('Ignoring attempt to stop capture when not running')
+            return
+        self._stop_future.set_result(None)
+        yield From(self._capture_future)
+        self._stop_future = None
+        self._capture_future = None
+
+    @trollius.coroutine
+    def _capture(self):
+        try:
+            wall_time = self._loop.time()
+            index = 0
+            while True:
+                # TODO: compute visibilities
+                logging.info('Prepared dump %d', index)
+                # Sleep until either it is time to make the dump, or we are asked
+                # to stop.
+                wall_time += self.subarray.accumulation_length
+                try:
+                    yield From(wait_until(trollius.shield(self._stop_future), wall_time, self._loop))
+                except trollius.TimeoutError:
+                    # This is the normal case: time for the next dump to be transmitted
+                    pass
+                else:
+                    # The _stop_future was triggered
+                    break
+                # TODO: emit the visibilities
+                index += 1
+            logging.info('Capture stopped by request')
+        except Exception:
+            logger.error('Exception in capture coroutine', exc_info=True)
--- katcbfsim/server.py
+++ katcbfsim/server.py
@@ -1,6 +1,4 @@
 from __future__ import print_function, division
-import time
-import copy
 import trollius
 from trollius import From
 import katcp
@@ -8,102 +6,10 @@ import tornado
 import logging
 from katcp.kattypes import Str, Float, Int, Address, request, return_reply
 from katsdptelstate import endpoint
+from .product import Subarray, Product
 
 
 logger = logging.getLogger(__name__)
-
-
-@trollius.coroutine
-def wait_until(future, when, loop=None):
-    """Like :meth:`trollius.wait_for`, but with an absolute timeout."""
-    def ready(*args):
-        if not waiter.done():
-            waiter.set_result(None)
-
-    if loop is None:
-        loop = trollius.get_event_loop()
-    waiter = trollius.Future(loop=loop)
-    timeout_handle = loop.call_at(when, ready)
-    # Ensure the that future is really a future, not a coroutine object
-    future = trollius.async(future, loop=loop)
-    future.add_done_callback(ready)
-    try:
-        result = yield From(waiter)
-        if future.done():
-            raise trollius.Return(future.result())
-        else:
-            future.remove_done_callback(ready)
-            future.cancel()
-            raise trollius.TimeoutError()
-    finally:
-        timeout_handle.cancel()
-
-
-class Subarray(object):
-    def __init__(self):
-        self.antennas = []
-        self.sync_time = time.time()
-        self.accumulation_length = 0.5
-
-
-class Product(object):
-    def __init__(self, subarray, name, bandwidth, center_frequency, channels):
-        self.name = name
-        self.subarray = subarray
-        self.destination = [endpoint.Endpoint('0.0.0.0', 0)]
-        self.bandwidth = bandwidth
-        self.center_frequency = center_frequency
-        self.channels = channels
-        self._capture_future = None
-        self._stop_future = None
-
-    def set_destination(self, destination):
-        if isinstance(destination, str):
-            destination = endpoint.endpoint_list_parser(0)(destination)
-        self.destination = destination
-
-    def capture_start(self):
-        if self._capture_future is not None:
-            logger.warn('Ignoring attempt to start capture when already running')
-            return
-        self._stop_future = trollius.Future()
-        self._capture_future = trollius.async(self._capture())
-
-    @trollius.coroutine
-    def capture_stop(self):
-        if self._capture_future is None:
-            logger.warn('Ignoring attempt to stop capture when not running')
-            return
-        self._stop_future.set_result(None)
-        yield From(self._capture_future)
-        self._stop_future = None
-        self._capture_future = None
-
-    @trollius.coroutine
-    def _capture(self):
-        try:
-            loop = trollius.get_event_loop()
-            wall_time = loop.time()
-            index = 0
-            while True:
-                # TODO: compute visibilities
-                print('Prepared dump', index)
-                # Sleep until either it is time to make the dump, or we are asked
-                # to stop.
-                wall_time += self.subarray.accumulation_length
-                try:
-                    yield From(wait_until(trollius.shield(self._stop_future), wall_time, loop))
-                except trollius.TimeoutError:
-                    # This is the normal case: time for the next dump to be transmitted
-                    pass
-                else:
-                    # The _stop_future was triggered
-                    break
-                # TODO: emit the visibilities
-                index += 1
-            print('Capture stopped by request')
-        except Exception:
-            logger.error('Exception in capture coroutine', exc_info=True)
 
 
 class SimulatorServer(katcp.DeviceServer):
@@ -119,32 +25,45 @@ class SimulatorServer(katcp.DeviceServer):
     def setup_sensors(self):
         pass
 
-    @request(Str(), Int(), Int(), Int())
+    @request(Str(), Int(), Int())
     @return_reply()
-    def request_instrument_create_correlator(self, sock, name, bandwidth, center_frequency, channels):
-        """Create a new simulated correlator production"""
+    def request_product_create_correlator(self, sock, name, bandwidth, channels):
+        """Create a new simulated correlator productt"""
         if name in self.products:
             return 'fail', 'product {} already exists'.format(name)
-        self.products[name] = Product(self.subarray, name, bandwidth, center_frequency, channels)
+        self.products[name] = Product(self.subarray, name, bandwidth, channels)
+        return 'ok',
+
+    @request(Str())
+    @return_reply()
+    def request_product_activate(self, sock, name):
+        """Activate a product created with product-create-correlator."""
+        try:
+            product = self.products[name]
+        except KeyError:
+            return 'fail', 'requested product name not found'
+        product.activate()
         return 'ok',
 
     @request(Str(), Address())
     @return_reply()
-    def request_capture_destination(self, sock, stream, destination):
-        """Set the destination endpoints for a stream"""
-        if stream not in self.products:
-            return 'fail', 'stream {} not in product list'.format(stream)
-        self.products[stream].set_destination(destination)
+    def request_capture_destination(self, sock, name, destination):
+        """Set the destination endpoints for a product"""
+        try:
+            product = self.products[name]
+        except KeyError:
+            return 'fail', 'requested product name not found'
+        product.destination = endpoint.endpoint_list_parser(0)(destination)
         return 'ok',
 
     @request(Str(default=''))
     @return_reply()
-    def request_capture_list(self, sock, stream):
-        """List the destination endpoints for a stream, or all streams"""
-        if stream != '' and stream not in self.products:
+    def request_capture_list(self, sock, req_name):
+        """List the destination endpoints for a product, or all products"""
+        if req_name != '' and req_name not in self.products:
             return 'fail', 'requested product name not found'
         for name, product in self.products.items():
-            if stream == '' or stream == name:
+            if req_name == '' or req_name == name:
                 # TODO: Add a formatter to katsdptelstate.endpoint that
                 # reconstructs the a.b.c.d+N:port format.
                 sock.inform(','.join([str(x) for x in product.destination]))
@@ -168,12 +87,12 @@ class SimulatorServer(katcp.DeviceServer):
 
     @request(Str(), Float())
     @return_reply()
-    def request_frequency_select(self, sock, stream, frequency):
+    def request_frequency_select(self, sock, name, frequency):
         """Set the center frequency for the band. Unlike the real CBF, an
         arbitrary frequency may be selected, and it will not be rounded.
         """
         try:
-            product = self.products[stream]
+            product = self.products[name]
         except KeyError:
             return 'fail', 'requested product name not found'
         product.center_frequency = center_frequency
@@ -205,6 +124,8 @@ class SimulatorServer(katcp.DeviceServer):
             product = self.products[stream]
         except KeyError:
             return 'fail', 'requested product name not found'
+        if not product.active:
+            return 'fail', 'requested product has not been activated'
         product.capture_start()
         return 'ok',
 
