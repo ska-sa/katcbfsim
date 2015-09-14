3e010a62742d3503b6398b63c1597164823ec067
Bruce Merry <bmerry@ska.ac.za>
2015-09-14 11:36:55 +0200

    Rename cbfsim -> katcbfsim in package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     author='Bruce Merry',
     author_email='bmerry@ska.ac.za',
-    name='cbfsim',
+    name='katcbfsim',
     version='0.1.dev0',
     description='Simulator for MeerKAT correlator',
     install_requires=['katcp', 'trollius', 'tornado>=4.2', 'spead2', 'katpoint', 'numpy',
