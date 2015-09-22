a3656786baa70466adaa15586780a4b75c8a50c3
Bruce Merry <bmerry@ska.ac.za>
2015-09-22 13:55:52 +0200

    Use reference position as reference antenna
    Previously, the position of the first antenna was used as a reference
    antenna. With this change, baseline_towards can just return the stored
    ENU offset in cases where the whole array is referenced to the same
    position. That should be a little faster, and may be more accurate.

--- katcbfsim/rime.py
+++ katcbfsim/rime.py
@@ -152,7 +152,10 @@ class Rime(accel.Operation):
         This performs an **asynchronous** transfer to the GPU, and the caller
         must wait for it to complete before calling the function again.
         """
-        ref_antenna = self.antennas[0]
+        # Create a reference antenna from the reference position of the first
+        # antenna. If all the antennas use the same reference point, this
+        # allows for a faster path through katpoint.Target.uvw.
+        ref_antenna = katpoint.Antenna('', *self.antennas[0].ref_position_wgs84)
         u, v, w = zip(*[self.phase_center.uvw(antenna, self.time, ref_antenna)
                         for antenna in self.antennas])
         ra, dec = zip(*[source.radec(self.time, ref_antenna) for source in self.sources])
