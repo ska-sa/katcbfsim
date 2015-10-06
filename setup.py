c654e063b4a8dab6dd18f8c3c068badb7c1fa435
Bruce Merry <bmerry@ska.ac.za>
2015-10-06 14:17:44 +0200

    Install the mako templates

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     author_email='bmerry@ska.ac.za',
     name='katcbfsim',
     version='0.1.dev0',
+    package_data={'': ['*.mako']},
+    include_package_data=True,
     description='Simulator for MeerKAT correlator',
     scripts=['scripts/cbfsim.py'],
     install_requires=['katcp', 'trollius', 'tornado>=4.2', 'spead2', 'katpoint', 'numpy', 'h5py',
