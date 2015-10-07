44210eaec64113b2b975ea216b5dde35d83ac6a6
Bruce Merry <bmerry@ska.ac.za>
2015-10-07 15:23:22 +0200

    Add script to build docs under Jenkins

---
+++ jenkins-build-doc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e -x
+pip install -U pip setuptools wheel
+pip install numpy  # Some requirements need it already installed
+pip install -r requirements.txt
+pip install .
+rm -rf doc/_build
+make -C doc html
