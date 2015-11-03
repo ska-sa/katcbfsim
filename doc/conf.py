9b7758f1fecd637905be7377d865e31dd557fe06
Bruce Merry <bmerry@ska.ac.za>
2015-11-03 11:37:57 +0200

    Switch from sphinxcontrib.napoleon to sphinx.ext.napoleon

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
