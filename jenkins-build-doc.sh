686b81ed327400bf7429079c7aa6fe4c1cade2d3
Bruce Merry <bmerry@ska.ac.za>
2016-07-27 08:57:55 +0200

    Remove old Jenkins scripts

--- jenkins-build-doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e -x
-pip install -r ~/docker-base/pre-requirements.txt
-install-requirements.py -d ~/docker-base/base-requirements.txt -d ~/docker-base/gpu-requirements.txt -r requirements.txt
-install-requirements.py -d ~/docker-base/base-requirements.txt -d ~/docker-base/gpu-requirements.txt -r doc-requirements.txt
-pip install --no-index .[doc]
-rm -rf doc/_build
-make -C doc html
--- jenkins-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e -x
-pip install -r ~/docker-base/pre-requirements.txt
-install-requirements.py -d ~/docker-base/base-requirements.txt -d ~/docker-base/gpu-requirements.txt \
-    -r requirements.txt -r test-requirements.txt
-export CUDA_DEVICE=0
-nosetests --with-xunit --with-coverage --cover-erase --cover-xml --cover-inclusive --cover-package=katcbfsim
